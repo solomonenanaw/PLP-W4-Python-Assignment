cb10eb38d023d6d17c5b1fbbe5d7bf6dfadb64bd
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2025-08-26 21:44:21 +0300

    Errorhandling.py

--- Errorhandling.py
+++ Errorhandling.py
@@ -1,1 +1,18 @@
+filename = input("Enter a file to read: ")
+
+try:
+    with open(filename, "r") as f:
+        lines = [line.upper() for line in f]
+
+    with open("output.txt", "w") as f:
+        f.writelines(lines)
+
+    print("Modified content to be wrriten 'output.txt'.")
+
+except FileNotFoundError:
+    print(f"Error: File '{filename}' not found.")
+except PermissionError:
+    print(f"Error: Permission denied unable to read '{filename}'.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
 
