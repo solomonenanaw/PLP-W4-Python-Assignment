ef96dd34e9c5380b0078e22e812d94eba8f3d572
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2025-08-26 21:40:28 +0300

    filReadwrite.py

--- filReadwrite.py
+++ filReadwrite.py
@@ -1,1 +1,14 @@
 
+import os
+
+if not os.path.exists("input.txt"):
+    with open("input.txt", "w") as f:
+        f.write("Hello world\nThis is a test.")
+
+with open("input.txt", "r") as f:
+    lines = [line.upper() for line in f]
+
+with open("output.txt", "w") as f:
+    f.writelines(lines)
+
+print("Modified content written to 'output.txt'.")
